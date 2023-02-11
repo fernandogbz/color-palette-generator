eda378d941ea06c54e9cf00b1e6f01c9b6fe3351
Fernando Jimenez <fgjv06@gmail.com>
2023-02-11 15:44:42 -0300

    JS - createPallete() created

---
+++ script.js
@@ -0,0 +1,13 @@
+const palleteContainer = document.getElementById('palleteContainer');
+const colorValues = ['1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'];
+const palleteSize = 5;
+
+const createPallete = () => {
+  for(let i = 0; i < palleteSize; i++) {
+    const palleteElement = document.createElement('div');
+    palleteElement.classList.add('palleteItem');
+    palleteContainer.appendChild(palleteElement);
+  }
+}
+
+createPallete();
