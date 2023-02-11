2da68262c60022c02fd7f0c1024252610aef6e1e
Fernando Jimenez <fgjv06@gmail.com>
2023-02-11 16:14:22 -0300

    JS - colorize() created

--- script.js
+++ script.js
@@ -11,6 +11,16 @@ const createPallete = () => {
   updatePallete();
 }
 
+const colorize = (element) => {
+  let color = '#';
+  for(let i = 0; i < 6; i++) {
+    const randomElement = colorValues[Math.floor(Math.random() * colorValues.length)];
+    color += randomElement;
+  }
+  element.style.backgroundColor = color;
+  element.innerHTML = `<span class='colorHex'>${color}</span>`;
+}
+
 const updatePallete = () => {
   for(let i = 0; i < palleteContainer.children.length; i++) {
     colorize(palleteContainer.children[i])
